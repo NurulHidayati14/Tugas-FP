d730f794ac4d7c59802a205c93689c7452784789
EristaNova <eristanovas@gmail.com>
2021-04-27 15:12:09 +0700

    nurul

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layout/main')
+
+@section('title', 'uyul tp bkn tuyul')
+
+@section('container')
+
+  <div class="container">
+    <div class="row">
+      <div class="col-10">
+        <div class="card">
+  <div class="card-header">
+  </div>
+  <h1 class="mt-2">18090071 Nurul Hidayati 6A</h1>
+<img src="{{asset('nurul.gif') }}" height="300px" width="350px;"/>
+  <div class="card-body">
+    <h5 class="card-title">password ...... on process</h5>
+     <div class="spinner-border text-primary" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+    </div>
+  </div>
+   </div>
+</div>
+@endsection
